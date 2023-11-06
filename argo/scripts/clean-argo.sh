cee9ef5e2032a1af945d3891dcf09437f19d4d88
dongbin Shin <dongbin4013@naver.com>
2023-11-06 17:25:15 +0900

    kiali 추가

--- argo/scripts/clean-argo.sh
+++ argo/scripts/clean-argo.sh
@@ -3,3 +3,6 @@
 helm -n argocd uninstall argocd
 helm -n argocd uninstall argo-apps
 kubectl delete ns argocd
+kubectl delete ns bookinfo
+kubectl delete ns istio-ingress
+kubectl delete ns istio-system
